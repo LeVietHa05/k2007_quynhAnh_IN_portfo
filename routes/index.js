25c69e1fc20d86b50e1828db947666da2a0014c0
LeVietHa05 <ha.leviet.0504@gmail.com>
2024-10-28 21:05:21 +0700

    add animation and create new file

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,23 @@ router.get('/insideout', function (req, res, next) {
   res.sendFile('views/insideout.html', { root: 'public' });
 });
 
+router.get('/battrang', function (req, res, next) {
+  res.sendFile('views/battrang.html', { root: 'public' });
+});
+router.get('/vanphuc', function (req, res, next) {
+  res.sendFile('views/vanphuc.html', { root: 'public' });
+});
+router.get('/thachxa', function (req, res, next) {
+  res.sendFile('views/thachxa.html', { root: 'public' });
+});
+router.get('/vanphuc2', function (req, res, next) {
+  res.sendFile('views/vanphuc2.html', { root: 'public' });
+});
+router.get('/lemat', function (req, res, next) {
+  res.sendFile('views/lemat.html', { root: 'public' });
+});
+router.get('/nhattan', function (req, res, next) {
+  res.sendFile('views/nhattan.html', { root: 'public' });
+});
+
 module.exports = router;
