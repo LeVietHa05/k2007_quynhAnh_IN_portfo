27b12c862883b655f3188e37ba8a1b24c1980f60
LeVietHa05 <ha.leviet.0504@gmail.com>
2024-11-03 15:40:28 +0700

    udpate new layout for test

--- public/javascripts/trace.js
+++ public/javascripts/trace.js
@@ -1,73 +1,52 @@
-let currentBlock = 1;
-let currentBlockImage = 0;
-let preview = document.getElementById('preview');
-let poemPreview = document.getElementById('poem-preview');
-let poemContainers = document.querySelectorAll('.poem-container');
-let poemPrev = document.querySelector('.poem-prev');
-let poemNext = document.querySelector('.poem-next');
+let currentIndex = 1;
 
-poemContainers.forEach((poemContainer, index) => {
-    poemContainer.addEventListener('click', () => {
-        preview.style.display = 'flex';
-        // poemPreview.classList.add('row-center');
-        console.log(currentBlock);
-        poemPrev.style.display = 'block';
-        poemPrev.style.opacity = 1;
-        poemNext.style.display = 'block';
-        poemNext.style.opacity = 1;
-        poemPreview.innerHTML =
-            `
-            <div class='p20 bg-milk col-center justify-start max-width-60p' style='overflow-y:auto ;max-height: 90%;margin: 10px 0;'>
-                <img style='width:100%' src="${blocks[currentBlock].imgs[currentBlockImage]}" alt="">
-                <p class='h6-2nd'>${blocks[currentBlock].content}</p>
-            </div>
-        `
-    })
-})
-poemPrev.addEventListener('click', () => {
-    console.log('clicked prev');
-    if (currentBlock == 1 && currentBlockImage == 0) {
-        preview.style.display = 'none';
-        return;
-    }
-    if (currentBlockImage == 0) {
-        currentBlock--;
-        currentBlockImage = blocks[currentBlock].imgs.length - 1;
+function showImage(index) {
+    currentIndex = index;
+    let mainImage = document.getElementById("main-image");
+    mainImage.src = images[index - 1].src;
+    if (images[index - 1].type == 'portrait') {
+        mainImage.style.width = 'auto';
+        mainImage.style.height = '100%';
     } else {
-        currentBlockImage--;
+        mainImage.style.width = '100%';
+        mainImage.style.height = 'auto';
     }
-    poemPreview.innerHTML =
-        `
-        <div class='p20 bg-milk col-center justify-start max-width-60p'  style='overflow-y:auto ;max-height: 90%;margin: 10px 0;'>
-            <img style='width:100%' src="${blocks[currentBlock].imgs[currentBlockImage]}" alt="">
-            <p class='h6-2nd '>${blocks[currentBlock].content}</p>
-        </div>
-    `
-})
-poemNext.addEventListener('click', () => {
-    console.log('clicked next');
-    if (currentBlock == Object.keys(blocks).length && currentBlockImage == blocks[currentBlock].imgs.length - 1) {
-        preview.style.display = 'none';
-        return;
-    }
-    if (currentBlockImage == blocks[currentBlock].imgs.length - 1) {
-        currentBlockImage = 0;
-        currentBlock++;
-    } else {
-        currentBlockImage++;
-    }
-    poemPreview.innerHTML =
-        `
-        <div class='p20 bg-milk col-center justify-start max-width-60p'  style='overflow-y:auto ;max-height: 90%; margin: 10px 0;'>
-            <img style='width:100%' src="${blocks[currentBlock].imgs[currentBlockImage]}" alt="">
-            <p class='h6-2nd '>${blocks[currentBlock].content}</p>
-        </div>
-    `
-})
+}
 
-preview.addEventListener('click', (e) => {
-    if (e.target === preview) {
-        preview.style.display = 'none';
-        // currentBlock = 0;
+function changeSlide(step) {
+    currentIndex += step;
+    if (currentIndex > images.length) {
+        currentIndex = 1;
+    } else if (currentIndex < 1) {
+        currentIndex = images.length;
     }
-})
+    showImage(currentIndex);
+}
+
+function genPreview() {
+    let previewContainer = document.querySelector(".preview-container");
+
+    for (let i = 0; i < images.length; i++) {
+        let previewDiv = document.createElement("div");
+        previewDiv.className = "preview";
+        let previewImage = document.createElement("img");
+        previewImage.src = images[i].src;
+        previewImage.className = "preview-image";
+        previewImage.onclick = function () {
+            showImage(i + 1);
+        };
+        previewDiv.appendChild(previewImage);
+        previewContainer.appendChild(previewDiv);
+    }
+}
+
+for (let text of blocks) {
+    let block = document.createElement("div");
+    block.className = "block";
+    let content = document.createElement("p");
+    content.innerHTML = text;
+    block.appendChild(content);
+    document.querySelector(".text-content").appendChild(block);
+}
+
+genPreview();
